c3bd65dab10736412b45ae2883210513d5a653fa
Vedant Singh <vedu8051@gmail.com>
2022-12-18 04:03:31 +0530

    solutions

--- TrafficLights_cses.cpp
+++ TrafficLights_cses.cpp
@@ -4,15 +4,30 @@ typedef long long ll;
 using namespace std;
 
 int main(){
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
     ll x,n;cin>>x>>n;
-    ll low=0,high=x;
-    for(int i=0;i<n;i++){
-        ll z;cin>>z;
-        ll mid=(low+high)/2;
-        if (z>mid && z<high)high=z;
-        else if (z<=mid && z>low) low=z;
-        cout<<high-low<<" ";
+    set <ll> poles{0,x};
+
+
+    multiset <ll> sums{x};
+    for(int i=0;i<n;i++) {
+        ll pole;cin>>pole;
+        auto it1= poles.upper_bound(pole);
+        auto it2= it1;--it2;
+
+        sums.erase(sums.find(*it1-*it2));sums.insert(pole - *it2);
+
+        // second interval insert karna hai
+        sums.insert(*it1-pole);
+        poles.insert(pole);
+
+        auto res=sums.end(); --res;
+        cout<<*res<<" ";
+    }
     }
 
 
-}
+
+
